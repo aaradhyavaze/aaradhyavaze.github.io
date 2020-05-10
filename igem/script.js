305580e6cc3479b2226b958cab51aa1c9ec0a2fc
Aaradhya Vaze <48019866+aaradhyavaze@users.noreply.github.com>
2020-05-10 18:01:06 +0530

    Update script.js

--- igem/script.js
+++ igem/script.js
@@ -24,6 +24,10 @@ function keyPressed() {
     borks.push(new Robust(mouseX, mouseY))
     numRobusts += 1
   }
+  //remove this bit later ig
+  if (keyCode == RIGHT_ARROW) {
+    borks.push(new TopSecret(mouseX, mouseY))
+  }
 }
 
 function mousePressed() {
@@ -58,43 +62,64 @@ function sudoku() {
       if (diffmag < 10) {
         // console.log('close!')
         // console.log(borks[i].identity(), borks[j+1].identity())
-        if (borks[i].identity() == 1 && borks[j+1].identity() == 0) {
+        if ((borks[i].identity() == 1 && borks[j+1].identity() == 0)
+          || (borks[i].identity() == 1 && borks[j+1].secretCode == true))
+           {
           //remove borks[i] and reduce antibiotic count.
           borks.splice(i, 1)
           numAntibiotics -= 1
           // whew! that worked!
           //insert capture sound
         }
-        else if (borks[i].identity() == 0 && borks[j+1].identity() == 1) {
+        else if ((borks[i].identity() == 0 && borks[j+1].identity() == 1)
+               || borks[i].secretCode == true && borks[j+1].identity() == 1) {
           borks.splice(j+1, 1)
           numAntibiotics -= 1
           //insert capture sound
         }
         //conjugation starts here:
+        //but checks happen before!
 
         // ecoli conjugates with borkteria
         // borkteria conju babys with borkteria
         // ecoli conjugates with ecoli
 
         else if (borks[i].identity() == 0 && borks[j+1].identity() == 2) {
-          console.log('bonju baby')
-          if (Math.random() < 0.1) {
             // conjugation happened: borteria got top secret and is now resistant
             // change color to full red
             borks[j+1].secretCode = true;
+            console.log('bonju baby')
 
-          }
         }
+
         else if (borks[i].identity() == 2 && borks[j+1].identity() == 0) {
-          console.log('baby bonju')
-          borks[i].secretCode = true;
+            console.log('baby bonju')
+            borks[i].secretCode = true;
+
+
+
         }
+
+        else if (borks[i].secretCode == true && borks[j+1].identity() == 2 &&
+      borks[j+1].secretCode == false) {
+          console.log('bonju bonju')
+          borks[j+1].secretCode = true;
+
+        }
+
+        else if (borks[i].identity() == 2 && borks[j+1].secretCode == true &&
+      borks[i].secretCode == false) {
+        borks[i].secretCode == true;
+
+
+      }
+
       }
     }
   }
   // i should probably put this in sudoku, instead of executing another function
   // also yay this seems to work!
-  if (numEcoli > 50) {
+  if (numEcoli > 25) {
     for (let i = 0 ; i < borks.length; i ++) {
       if (borks[i].identity() == 0) {
         borks.splice(i, 1)
@@ -102,6 +127,15 @@ function sudoku() {
         break
       }
       console.log('not an ecoli yet.')
+    }
+  }
+  if (numRobusts > 25) {
+    for (let i = 0; i < borks.length; i ++) {
+      if (borks[i].identity() == 2) {
+        borks.splice(i, 1)
+        numRobusts -= 1;
+        break;
+      }
     }
   }
 }
@@ -133,11 +167,21 @@ function draw() {
   sudoku();
   // bork.hajimefy()
   // bork.over();
+  let numResistant = 0
+  for (let i of borks) {
+    if (i.secretCode == true) {
+      numResistant += 1
+    }
+  }
+
   fill(255)
   textFont('monospace')
   strokeWeight(0.3)
+  stroke(0)
   textAlign(CENTER)
-  text(` E.Coli: ${numEcoli}`, width/2, height-50)
-  text(` Antibiotics: ${numAntibiotics}`, width/2, height-30)
-  text(` Random borkteria: ${numRobusts}`, width/2, height - 10)
+  text(` E.Coli: ${numEcoli}`, width/2, height-70)
+  text(` Antibiotics: ${numAntibiotics}`, width/2, height-50)
+  text(` Random borkteria: ${numRobusts}`, width/2, height - 30)
+  fill(255, 100, 50, 200)
+  text(` Resistant: ${Math.round(numResistant/numRobusts*100)}%`, width/2, height - 10)
 }
