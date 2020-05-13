363ab2927f5247bb00a943abe873ac7692b1ecf3
Aaradhya Vaze <48019866+aaradhyavaze@users.noreply.github.com>
2020-05-13 06:58:49 +0530

    Update script.js

--- igem/script.js
+++ igem/script.js
@@ -443,7 +443,7 @@ function draw() {
     textSize(15)
     text( `2. And then it can conjugate with an F- bacteria (yellow) and give IT the code that kills antibiotic`, width/2, 220)
     textSize(13)
-    text( `Bring F+ E-Coli close to yellow bacteria. If it becomes red, it is now Resistant`, width/2, 240)
+    text( `Bring F+ E-Coli close to yellow bacteria (takes a couple of tries). If it becomes red, it is now Resistant`, width/2, 240)
     textSize(15)
     text(`Enter to continue`, width/2, 930)
 
@@ -469,7 +469,7 @@ if (currentPage == 3) {
   textAlign(CENTER)
   text(`one more problem: sometimes when E-Coli dies, it releases its genetic code in the environment! ` , width/2, 70)
   textSize(13)
-  text( `wait for it to die or create one with right arrow`, width/2, 90)
+  text( `wait for it to die or create one with the right arrow`, width/2, 90)
   textSize(15)
   text(`this genetic material can be taken up by random bacteria, and can gain Resistance`, width/2, 120)
   textSize(13)
