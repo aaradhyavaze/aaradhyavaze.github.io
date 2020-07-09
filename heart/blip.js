cb7512941f879dc0cecbcf98e7e2a89ead4ef53d
Aaradhya Vaze <48019866+aaradhyavaze@users.noreply.github.com>
2020-07-09 10:23:30 +0530

    grid and snap

--- heart/blip.js
+++ heart/blip.js
@@ -3,6 +3,7 @@
 // let steps = 0;
 // let r = 1
 let amt = 2, steps = -1, r = 1
+let delta = 50
 //imma do a gui
 class St {
   constructor() {
@@ -22,6 +23,19 @@ class Point {
   }
 }
 
+let ver = []
+let cols = [[238, 66, 102], [96, 211, 148], [252, 171, 16], [84, 19, 136], [35, 100, 170]]
+let sw = 0
+keyPressed = () => {
+  sw = keyCode === DOWN_ARROW ? 1 : 0
+
+  if (keyCode === UP_ARROW && sw != 1) ver.push(new Point(rounder(mouseX), rounder(mouseY)))
+}
+
+function rounder(n) {
+  return delta*Math.ceil(n/delta)
+}
+
 setup = () => {
   createCanvas(windowWidth, windowHeight)
   background(250)
@@ -30,22 +44,12 @@ setup = () => {
   st = new St();
   gui.add(st, 'amt')
   gui.add(st, 'steps')
+  stroke(0, 100, 0, 150)
+  for (let i = 0; i < width+1 || i < height +1; i+= 50) {
+    line(i, 0, i, height)
+    line(0, i, width, i)
+  }
 }
-
-let ver = []
-let cols = [[238, 66, 102], [96, 211, 148], [252, 171, 16], [84, 19, 136], [35, 100, 170]]
-let sw = 0
-keyPressed = () => {
-  sw = keyCode === DOWN_ARROW ? 1 : 0
-  if (keyCode === UP_ARROW && sw != 1) ver.push(new Point(mouseX, mouseY))
-}
-// mousePressed = () => {
-//   if (sw != 1) ver.push(new Point(mouseX, mouseY))
-// }
-
-//some more globals:
-//declare an r now, rpr becomes this r, then i check (one point mistake doesn't matter in the long run)
-//
 
 // let zo = (lo) => ((createVector((p.x + lo.x)/2, (p.y + lo.y)/2)))
 let zo = (lo) => (p5.Vector.lerp(p, lo, 1/st.amt))
@@ -62,15 +66,13 @@ function draw() {
   //draw p with cols
   // c = (cols.length < r0.length) ?  : (
   if (r0.length != 0) {
-    strokeWeight(4)
+    strokeWeight(6)
     stroke(cols[r%5])
     point(p.x, p.y)
   }
 
-  //normal version:
-  // p = sw === 1 ? zo(ver[r].pos) : p
-
   //actually ig i'll generalise this to (new vertex cannot be n steps from current vertex)
   //so steps -1 i can put for the normal case xool
+
 
 }
